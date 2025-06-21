305386247963b98d81e8ce079d169543445a90de
mechseiko <mechseiko@gmail.com>
2025-06-21 01:02:43 +0100

    Loading Effect

--- src/components/Lander/Lander.jsx
+++ src/components/Lander/Lander.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Header';
 import Hero from './Hero';
 import Location from '../Location';
 import Footer from '../Footer';
-import {motion} from 'framer-motion'
+import Loader from '../Loader';
 
 function Lander(props) {
+    const [loading, setLoading] = useState(true);
+    const loader = setInterval(() => {
+        setLoading(false);
+        clearInterval(loader);
+    }, 2000);
     return (
         <div>
-            <Header/>
-            <Hero/>
-            <Location/>
-            <Footer/>
+            {
+                loading ? <Loader/> : 
+                <>
+                    <Header/>
+                    <Hero/>
+                    <Location/>
+                    <Footer/>
+                </>
+            }
         </div>
     );
 }
