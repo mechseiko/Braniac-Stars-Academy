9a310139df5758c24d8926f5644fb211d0db819e
mechseiko <mechseiko@gmail.com>
2025-06-30 15:58:29 +0100

    Modified Error 404 page

--- src/components/Lost.jsx
+++ src/components/Lost.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Lost = () => {
     return (
         <div>
-            You are lost in space...
+            The Page you are looking for does not exist.
         </div>
     );
 }
