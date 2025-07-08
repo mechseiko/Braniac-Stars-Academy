44b10c3e65252922d146976569c6a27299cc37b4
mechseiko <mechseiko@gmail.com>
2025-07-08 10:56:51 +0100

    Development postponed till after exams

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import {motion} from 'framer-motion'
 const Footer = () => {
     return (
         <motion.footer style={{textAlign:"center", backgroundColor:"blanchedalmond", padding: "20px", fontWeight:700,}}>
-            <h3>Copyright &copy; Brainiac Stars Academy 2022 - {new Date().getFullYear()}</h3>
-            <h4>Developed by <a style={{textDecoration:"none",}} href="">MECHSEIKO</a></h4>
+            <h4>Copyright &copy; Brainiac Stars Academy 2022 - {new Date().getFullYear()}</h4>
+            <h5>Developed by <a style={{textDecoration:"none",}} href="">MECHSEIKO</a></h5>
         </motion.footer>
     );
 }
